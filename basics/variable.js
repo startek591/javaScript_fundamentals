8865315bfce3a5a07178d24f3085114f1a337282
Jonathan Fambro <startek591@gmail.com>
2022-11-03 15:13:02 -0400

    Different types of Datatypes

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+(function(){
+  var variableName = 'text';
+  console.log(variableName);
+  console.log(typeof variableName);
+
+  var var1 = 5;
+  console.log(var1);
+  console.log(typeof var1)
+}());
