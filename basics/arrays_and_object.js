85f0b659099a92688274418ee654dee8a054c9af
Jonathan Fambro <startek591@gmail.com>
2022-11-03 15:40:43 -0400

    Arrays and Objects

---
+++ basics/arrays_and_object.js
@@ -0,0 +1,17 @@
+(function(){
+  var var1 = [1, 2, 3];
+  console.log(var1);
+  console.log(typeof var1);
+
+  console.log(var1[0]);
+  console.log(var1[4]);
+
+  var var2 = {
+    name: 'Hello World',
+    age: 22
+  }
+
+  console.log(var2);
+  console.log(var2.name);
+  console.log(typeof var2);
+}());
