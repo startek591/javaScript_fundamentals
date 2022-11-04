8d961767531a6321218bc7066c465a7786be5956
Jonathan Fambro <startek591@gmail.com>
2022-11-04 17:26:17 -0400

    Switch Statement

---
+++ basics/09_switch.js
@@ -0,0 +1,17 @@
+(function(){
+  let number = 5;
+  
+  switch(number) {
+    case 1: 
+    console.log('number is 1');
+    break;
+    case 2: 
+    console.log('number is 2');
+    break;
+    case 3:
+    console.log('number is 3');
+    break;
+    default:
+      console.log('nothing');
+  }
+}());
