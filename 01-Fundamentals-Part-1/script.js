8e423d1f9a300caaef538b9eebfa38ab471f8927
Jonathan Fambro <startek591@gmail.com>
2023-10-03 23:07:30 -0400

    Second part of the coding challege

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -109,10 +109,15 @@
     3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
   */
 
-  const massMark = 78;
-  const heightMark = 1.69;
-  const massJohn = 92;
-  const heightJohn = 1.95;
+  // const massMark = 78;
+  // const heightMark = 1.69;
+  // const massJohn = 92;
+  // const heightJohn = 1.95;
+
+  const massMark = 95;
+  const heightMark = 1.88;
+  const massJohn = 85;
+  const heightJohn = 1.76;
 
   const BMIMark = massMark / heightMark ** 2;
   const BMIJohn = massJohn / (heightJohn * heightJohn);
