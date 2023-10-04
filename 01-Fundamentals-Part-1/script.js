9ba075b491ba7b10b9053efb88fab35593d2a4be
Jonathan Fambro <startek591@gmail.com>
2023-10-03 22:36:05 -0400

    let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -48,4 +48,20 @@
   year = 1991;
   console.log(typeof year);
   console.log(typeof null);
+
+  // let, const and var
+  let age = 30;
+  age = 31;
+
+  const birthYear = 1991;
+  // birthYear = 1990;
+  // const job;
+
+  var job = "programmer";
+  job = "teacher";
+
+  lastName = "Schmedtmann";
+  console.log(birthYear);
+  console.log(job);
+  console.log(lastName);
 })();
