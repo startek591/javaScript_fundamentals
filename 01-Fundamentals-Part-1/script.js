c51c9a59351d9883ee6ff034f6d6efcf5c615a92
Jonathan Fambro <startek591@gmail.com>
2023-10-04 14:00:40 -0400

    Closing of the Fundamental Part 1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -272,16 +272,13 @@
   // }
   // const me = "Jonas";
   // console.log(`I'm ${2037 - 1991} years old ${me}`);
-
   // The Conditional (Ternary) Operator
   // const age = 23;
   // age >= 18
   //   ? console.log("I like to drink wine 🍷")
   //   : console.log("I like to drink water 💧");
-
   // const drink = age >= 18 ? "wine 🍷" : "water 💧";
   // console.log(drink);
-
   // let drink2;
   // if (age >= 18) {
   //   drink2 = "wine 🍷";
@@ -289,9 +286,7 @@
   //   drink2 = "water 💧";
   // }
   // console.log(drink2);
-
   // console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
-
   // Coding Challenge #4
   /*
   Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%;
@@ -304,9 +299,9 @@
   HINT: To calculate 20% of a value, simple multiply it by 20/100 = 0.2;
   HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
   */
-  const bill = 430;
-  const tip = bill <= 300 && bill >= 50 ? bill * 0.5 : bill * 0.2;
-  console.log(
-    `The bill was ${bill}, the tip was ${tip}, and total value ${bill + tip}.`
-  );
+  // const bill = 430;
+  // const tip = bill <= 300 && bill >= 50 ? bill * 0.5 : bill * 0.2;
+  // console.log(
+  //   `The bill was ${bill}, the tip was ${tip}, and total value ${bill + tip}.`
+  // );
 })();
