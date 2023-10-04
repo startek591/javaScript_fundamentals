161acc25e21df764ff7e512cc88b8eeb3deb3ffa
Jonathan Fambro <startek591@gmail.com>
2023-10-04 12:42:25 -0400

    == vs. ===

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -171,26 +171,46 @@
   // let n = "1" + 1; // '11'
   // n = n - 1;
   // console.log(n);
-
   // Truthy and Falsy Values
   // 5 falsy values: 0, '', undefined, null, NaN
-  console.log(Boolean(0));
-  console.log(Boolean(undefined));
-  console.log(Boolean("Jonas"));
-  console.log(Boolean({}));
-  console.log(Boolean(""));
+  // console.log(Boolean(0));
+  // console.log(Boolean(undefined));
+  // console.log(Boolean("Jonas"));
+  // console.log(Boolean({}));
+  // console.log(Boolean(""));
+  // const money = 100;
+  // if (money) {
+  //   console.log("Don't spend it all ");
+  // } else {
+  //   console.log("You should get a job!");
+  // }
+  // let height = 0;
+  // if (height) {
+  //   console.log("YAY! Height is defined");
+  // } else {
+  //   console.log("Height is UNDEFINED");
+  // }
 
-  const money = 100;
-  if (money) {
-    console.log("Don't spend it all ");
+  // Equality Operators: == vs. ===
+  const age = "18";
+  if (age === 18) console.log("You just became an adult :D (strict)");
+
+  if (age == 18) console.log("You just became an adult :D (loose)");
+
+  const favourite = Number(prompt("What's your favourite number?"));
+  console.log(favourite);
+  console.log(typeof favourite);
+
+  if (favourite === 23) {
+    // 22 === 23 -> FALSE
+    console.log("Cool! 23 is an amazing number!");
+  } else if (favourite === 7) {
+    console.log("7 is also a cool number");
+  } else if (favourite === 9) {
+    console.log("9 is also a cool number");
   } else {
-    console.log("You should get a job!");
+    console.log("Number is not 23 or 7 or 9");
   }
 
-  let height = 0;
-  if (height) {
-    console.log("YAY! Height is defined");
-  } else {
-    console.log("Height is UNDEFINED");
-  }
+  if (favourite !== 23) console.log("Why not 23?");
 })();
