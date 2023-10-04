0677a3d9e50707aa2aca8a70dd2ba5b51d80155c
Jonathan Fambro <startek591@gmail.com>
2023-10-03 23:05:55 -0400

    First part of the coding challenge

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -67,29 +67,55 @@
 
   // Basic Operators
   // Math operators
-  const now = 2037;
-  const ageJonas = now - 1991;
-  const ageSarah = now - 2018;
+  // const now = 2037;
+  // const ageJonas = now - 1991;
+  // const ageSarah = now - 2018;
 
-  console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+  // console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
   // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
 
-  const firstName = "Jonas";
-  const lastName = "Schmedtmann";
-  console.log(firstName + " " + lastName);
+  // const firstName = "Jonas";
+  // const lastName = "Schmedtmann";
+  // console.log(firstName + " " + lastName);
 
   // Assignment operators
-  let x = 10 + 5; // 15
-  x += 10; // x = x + 10 = 25
-  x *= 4; // x = x * 4 = 100
-  x++; // x = x + 1;
-  x--;
-  x--;
-  console.log(x);
+  // let x = 10 + 5; // 15
+  // x += 10; // x = x + 10 = 25
+  // x *= 4; // x = x * 4 = 100
+  // x++; // x = x + 1;
+  // x--;
+  // x--;
+  // console.log(x);
 
   // Comparison operators
-  console.log(ageJonas > ageSarah); // >, <, >=, <=
-  console.log(ageSarah >= 18);
+  // console.log(ageJonas > ageSarah); // >, <, >=, <=
+  // console.log(ageSarah >= 18);
 
-  console.log(now - 1991 > now - 2018);
+  // console.log(now - 1991 > now - 2018);
+
+  // let x, y;
+  // x;
+  // console.log(x, y);
+
+  // const averageAge = (ageJonas + ageSarah) / 2;
+  // console.log(ageJonas, ageSarah, averageAge);
+
+  // Coding Challenge #1
+  /*
+    Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+    1. Store Mark's and John's mass and height in variables
+    2. Calculate both their BMIs using the formula (you can implement both versions)
+    3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+  */
+
+  const massMark = 78;
+  const heightMark = 1.69;
+  const massJohn = 92;
+  const heightJohn = 1.95;
+
+  const BMIMark = massMark / heightMark ** 2;
+  const BMIJohn = massJohn / (heightJohn * heightJohn);
+  const markHigherBMI = BMIMark > BMIJohn;
+  console.log(BMIMark, BMIJohn, markHigherBMI);
 })();
