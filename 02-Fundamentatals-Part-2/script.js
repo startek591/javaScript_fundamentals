a42e74ce46d6dd0c5b25b6fe955ca6c715635921
Jonathan Fambro <startek591@gmail.com>
2023-10-05 13:07:53 -0400

    Coding Challenge #2 with arrow functions

--- 02-Fundamentatals-Part-2/script.js
+++ 02-Fundamentatals-Part-2/script.js
@@ -228,9 +228,12 @@ Steven is still building his tip calculator, using the same rules as before: Tip
 TEST DATA: 125, 55 and 44
 */
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
