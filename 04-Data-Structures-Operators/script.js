3ed2550b96f0487a49fb5b3f6fc31d0063dad9e0
Jonathan Fambro <startek591@gmail.com>
2023-10-30 16:48:55 -0400

    Maps: Iteration, Convert object to map

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -206,3 +206,31 @@ for (const flight of flights.split('+')) {
 
 // console.log(new String('jonas'));
 // console.log(typeof new String('jonas'));
+
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer: ${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get('correct') === answer);
