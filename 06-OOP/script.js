53c25682ca089e136dfb373b84bfa63126d662a6
Jonathan Fambro <startek591@gmail.com>
2023-11-07 10:26:16 -0500

    Classes Declaration

--- 06-OOP/script.js
+++ 06-OOP/script.js
@@ -1,72 +1,132 @@
 'use strict';
 
 // Constructor Functions and the new Operator
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+//   this.calcAge = function () {
+//     console.log(2037 - this.birthYear);
+//   };
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
+
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1975);
+
+// console.log(jonas instanceof Person);
+
+// Person.hey = function () {
+//   console.log('Hey there 👋');
+//   console.log(this);
+// };
+
+// Person.hey();
+
+// // Prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// jonas.calcAge();
+// matilda.calcAge();
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// // .prototypeOfLinkedObjects
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species, matilda.species);
+
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+
+// // Prototypal Inheritance on Built-In Objects
+// console.log(jonas.__proto__);
+// // Object.prototype (top of prototype chain)
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 6, 9, 9];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+// ES6 Classes
+// Class expression
+const PersonCl = class {};
+
+// Class declaration
+class PersonCl2 {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  // Methods will be added to .property property
+  calcAge() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+
+const jessica = new PersonCl2('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl2.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
 };
+jessica.greet();
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-
-console.log(jonas instanceof Person);
-
-Person.hey = function () {
-  console.log('Hey there 👋');
-  console.log(this);
-};
-
-Person.hey();
-
-// Prototypes
-console.log(Person.prototype);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
-
-jonas.calcAge();
-matilda.calcAge();
-
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-// .prototypeOfLinkedObjects
-
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, matilda.species);
-
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-
-// Prototypal Inheritance on Built-In Objects
-console.log(jonas.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 6, 5, 6, 9, 9];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+const walter = new PersonCl2('Walter White', 1965);
+PersonCl2.hey();
