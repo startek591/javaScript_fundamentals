c27fe042d2f52a525ce07a62110d44def6786597
Jonathan Fambro <startek591@gmail.com>
2023-11-07 10:46:19 -0500

    Example of classes' setters and getters

--- 06-OOP/script.js
+++ 06-OOP/script.js
@@ -130,3 +130,22 @@ jessica.greet();
 
 const walter = new PersonCl2('Walter White', 1965);
 PersonCl2.hey();
+
+// Setters and Getters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
