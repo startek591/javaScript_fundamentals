cfd1e137461d702c67857a853c300be5cef9449a
Jonathan Fambro <startek591@gmail.com>
2023-11-07 11:07:01 -0500

    Object create

--- 06-OOP/script.js
+++ 06-OOP/script.js
@@ -73,79 +73,102 @@
 
 // ES6 Classes
 // Class expression
-const PersonCl = class {};
+// const PersonCl = class {};
 
-// Class declaration
-class PersonCl2 {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// // Class declaration
+// class PersonCl2 {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // Instance methods
-  // Methods will be added to .property property
+//   // Instance methods
+//   // Methods will be added to .property property
+//   calcAge() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   // Static method
+//   static hey() {
+//     console.log('Hey there 👋');
+//     console.log(this);
+//   }
+// }
+
+// const jessica = new PersonCl2('Jessica Davis', 1996);
+// console.log(jessica);
+// jessica.calcAge();
+// console.log(jessica.age);
+
+// console.log(jessica.__proto__ === PersonCl2.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+// jessica.greet();
+
+// // 1. Classes are NOT hoisted
+// // 2. Classes are first-class citizens
+// // 3. Classes are executed in strict mode
+
+// const walter = new PersonCl2('Walter White', 1965);
+// PersonCl2.hey();
+
+// // Setters and Getters
+// const account = {
+//   owner: 'Jonas',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+
+// account.latest = 50;
+// console.log(account.movements);
+
+// Object.create
+const PersonProto = {
   calcAge() {
-    console.log(`Hey ${this.fullName}`);
-  }
-
-  greet() {
-    console.log(`Hey ${this.fullName}`);
-  }
-
-  get age() {
-    return 2037 - this.birthYear;
-  }
-
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a full name!`);
-  }
-
-  get fullName() {
-    return this._fullName;
-  }
-
-  // Static method
-  static hey() {
-    console.log('Hey there 👋');
-    console.log(this);
-  }
-}
-
-const jessica = new PersonCl2('Jessica Davis', 1996);
-console.log(jessica);
-jessica.calcAge();
-console.log(jessica.age);
-
-console.log(jessica.__proto__ === PersonCl2.prototype);
-
-PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
-};
-jessica.greet();
-
-// 1. Classes are NOT hoisted
-// 2. Classes are first-class citizens
-// 3. Classes are executed in strict mode
-
-const walter = new PersonCl2('Walter White', 1965);
-PersonCl2.hey();
-
-// Setters and Getters
-const account = {
-  owner: 'Jonas',
-  movements: [200, 530, 120, 300],
-
-  get latest() {
-    return this.movements.slice(-1).pop();
+    console.log(2037 - this.birthYear);
   },
 
-  set latest(mov) {
-    this.movements.push(mov);
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
   },
 };
 
-console.log(account.latest);
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+console.log(steven.__proto__ === PersonProto);
 
-account.latest = 50;
-console.log(account.movements);
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
