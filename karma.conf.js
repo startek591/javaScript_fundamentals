64ea93307582fdd71f5baefa0d6a56538121d8ba
jonathan_fambro <startek591@gmail.com>
2024-04-25 21:41:40 -0400

    Add dependencies and karma configuration files

---
+++ karma.conf.js
@@ -0,0 +1,51 @@
+module.exports = function (config) {
+  config.set({
+    // Base path that will be used to resolve files and exclude
+    basePath: '',
+
+    // Frameworks to use
+    frameworks: ['jasmine'],
+
+    // List of files/patterns to load in the browser
+    files: [
+      'app.js', // Include your source files
+      'app.spec.js', // Include your spec files
+    ],
+
+    // List of files/patterns to exclude from loading
+    exclude: [],
+
+    // Preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+    // Test results reporter to use
+    // possible values: 'dots', 'progress'
+    reporters: ['progress'],
+
+    // Web server port
+    port: 9876,
+
+    // Enable/disable colors in the output (reporters and logs)
+    colors: true,
+
+    // Level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Enable/disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // Start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+  });
+};
