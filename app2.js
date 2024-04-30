8cfbd67e966eb0949aef817ab235625edbb90799
jonathan_fambro <startek591@gmail.com>
2024-04-30 12:03:09 -0400

    Coding Challenge #2

--- app2.js
+++ app2.js
@@ -181,3 +181,36 @@ console.log(friends2.includes(23));
 if (friends.includes('Steven')) {
   console.log('You have a friend called Steven');
 }
+
+// Coding Challenge #2
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%;
+
+1. Write a function 'calcTip' that takes an any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Crete an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 55 and 44
+*/
+
+const calcTip1 = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const calcTip2 = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [
+  calcTip1(bills[0]),
+  calcTip2(bills[1]),
+  calcTip1(bills[2]),
+];
+const totals = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[2] + tips[2],
+];
+
+console.log(bills, tips, totals);
