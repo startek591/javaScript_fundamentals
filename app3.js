7d375e4d1902f9a313bbed2fd16349b3e624debd
jonathan_fambro <startek591@gmail.com>
2024-05-04 15:06:53 -0400

    Examples of Maps

--- app3.js
+++ app3.js
@@ -207,3 +207,56 @@ checkMiddleSeat('3E');
 
 console.log(new String('jonas'));
 console.log(typeof new String('jonas'));
+
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+]);
+console.log(question);
+
+// Converts objects to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+
+// Maps Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon', 'Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(
+  rest.get(time > rest.get('open') && time < rest.get('close'))
+);
+
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
