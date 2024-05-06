3fb312041865ac39471d6940416314711a5a8038
jonathan_fambro <startek591@gmail.com>
2024-05-06 13:32:27 -0400

    Created a Promise that get information from an API

---
+++ app6.js
@@ -0,0 +1,35 @@
+// Our First AJAX Call: XMLHttpRequest
+// Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
+// Throwing Errors Manually
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data[0];
+    });
+};
+
+const getCountryData1 = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
+    .then((data) => {
+      return data[0];
+    });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
+
+getCountryData1('portugal');
+getCountryData1('usa');
+getCountryData1('germany');
