85d2f97fea86b1389e1cf4ec296d82cd9e778939
jonathan_fambro <startek591@gmail.com>
2024-05-06 13:36:18 -0400

    Building a simple promise

--- app6.js
+++ app6.js
@@ -33,3 +33,19 @@ getCountryData('germany');
 getCountryData1('portugal');
 getCountryData1('usa');
 getCountryData1('germany');
+
+// Building a Simple Promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
