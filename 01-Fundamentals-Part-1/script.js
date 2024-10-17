c25b1f73775d065f762f4cf84c0b4c2da110bf1b
jonathan_fambro <startek591@gmail.com>
2024-10-16 23:10:46 -0400

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,12 @@
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 console.log(23);
+
+// Values and Variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
