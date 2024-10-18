c2c5c07817ee035b34842d67129fd63f3046d22d
jonathan_fambro <startek591@gmail.com>
2024-10-18 16:42:57 -0400

    The switch Statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -348,3 +348,43 @@ if (scoreDolphins2 > scoreKoalas2 && scoreDolphins2 >= 100) {
 } else {
   console.log('No one wins the trophy 😭');
 }
+
+// The switch Statement
+const day = 'friday';
+
+switch (day) {
+  case 'monday': // day == monday
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Write code examples');
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
+}
