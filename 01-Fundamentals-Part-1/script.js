6717b10920cba8c8edfbf5d832f40775a4e31896
jonathan_fambro <startek591@gmail.com>
2024-10-18 09:58:20 -0400

    Assignment operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -71,3 +71,11 @@ console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// Assignment operators
+let x = 10; // x = x + 10
+x += 10; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
