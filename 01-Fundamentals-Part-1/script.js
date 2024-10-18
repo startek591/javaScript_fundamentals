ee781157fbcefe7b4dc32447ac023f1bf63e84b5
jonathan_fambro <startek591@gmail.com>
2024-10-18 10:44:41 -0400

    Taking Decisions - if / else statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -158,3 +158,25 @@ console.log(`Just a regular string...`);
 console.log('String with \n lines');
 
 console.log(`String multiple lines`);
+
+// Taking Decisions: if / else Statements
+const age1 = 15;
+
+if (age >= 18) {
+  console.log('Sarah can start driving license 🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(
+    `Sarah is too young. Wait another ${yearsLeft} years :)`
+  );
+}
+
+const birthYear3 = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
