831bec076882449d38d1b674c3a0d0ba38aa16e7
jonathan_fambro <startek591@gmail.com>
2024-10-18 09:57:54 -0400

    Basic Operators - Math Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -61,3 +61,13 @@ job = 'teacher';
 
 lastName = 'Schmedtmann';
 console.log(lastName);
+
+// Basic Operators
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
