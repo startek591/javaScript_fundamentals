3bf6976fdb07e95ad8e79e1a049e07d1340deb72
jonathan_fambro <startek591@gmail.com>
2025-06-02 11:15:32 -0400

    Variable name conventions, and Data Types

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,40 @@
+// Linking a JavaScript File
+let js = "amazing";
+let value1 = 40 + 8 + 23 - 10;
+
+// Values and Variables
+let value2 = "Jonas";
+let value3 = 23;
+
+let firstName = "Matilda";
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+// Data Types
+let javascriptIsFun = true;
+
+let _typeOfBoolean = typeof true;
+let _typeOfNumber = typeof 23;
+let _typeOfString = typeof "Jonas";
+
+javascriptIsFun = "YES!";
+
+let year;
+let _typeOfUnderfind = year;
+
+year = 1991;
+
+let _typeOfNumber3 = year;
+
+let _typeOfNull = typeof null;
