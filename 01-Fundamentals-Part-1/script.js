36ba9b77b854c9ba1b66c1421dc58afdc4e5bd5a
jonathan_fambro <startek591@gmail.com>
2025-06-07 14:02:53 -0400

    Basic Operators, Math Operators, Assignment Operators, Comparison Operators and Order Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -49,3 +49,42 @@ var job3 = "programmer";
 job3 = "teacher";
 
 lastName = "Schmedtmann";
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+const power = ageJonas * 2 * (ageJonas / 10) * 2 ** 3;
+
+const firstName2 = "Jonas";
+const lastName2 = "Schmedtmann";
+const fullName = firstName2 + " " + lastName2;
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10;
+x *= 4;
+x++;
+x--;
+
+// Comparison operators
+let comparison1 = ageJonas > ageSarah;
+let comparison2 = ageSarah >= 18;
+
+const isFullAge = ageSarah >= 18;
+
+let comparison3 = now - 1991 > now - 2018;
+
+// Operator Precedence
+const now2 = 2037;
+const ageJonas2 = now2 - 1991;
+const ageSarah2 = now2 - 2018;
+
+let comparsion4 = now2 - 1991 > now2 - 2018;
+
+let x2, y2;
+x2 = y2 = 25 - 10 - 5;
+
+const averageAge = (ageJonas2 + ageSarah2) / 2;
