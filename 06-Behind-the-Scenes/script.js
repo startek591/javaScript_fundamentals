099b2d5ce59a71d70f1f88eddfdd788542ba1476
jonathan_fambro <startek591@gmail.com>
2025-06-30 10:47:00 -0400

    This

--- 06-Behind-the-Scenes/script.js
+++ 06-Behind-the-Scenes/script.js
@@ -64,3 +64,39 @@ function deleteShoppingCart() {
 var x = 1;
 let y = 2;
 const z = 3;
+
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge2 = function (birthYear) {
+  2037 - birthYear;
+  console.log(this);
+};
+
+calcAge2(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
