4d3cb12c83cb14c9394d7abe723ce238e68508a5
jonathan_fambro <startek591@gmail.com>
2025-07-05 00:37:18 -0400

    Optional Chaining, WITH optional chaining, Methods and Arrays

--- 07-Data-Structures-Operators/script.js
+++ 07-Data-Structures-Operators/script.js
@@ -341,3 +341,30 @@ console.log(entries);
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
+
+// Optional Chaining
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+// Arrays
+const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+console.log(users[0]?.name ?? "User array empty");
+
+if (users.length > 0) console.log(users[0].name);
+else console.log("user array empty");
