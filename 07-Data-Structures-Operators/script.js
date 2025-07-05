e8641cdc4a07577733ef48006f9aa002fa698d19
jonathan_fambro <startek591@gmail.com>
2025-07-05 10:33:18 -0400

    Spread for Objects

--- 07-Data-Structures-Operators/script.js
+++ 07-Data-Structures-Operators/script.js
@@ -459,3 +459,24 @@ const [pizza, , risotto, ...otherFood] = [
 ];
 
 console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays2 } = restaurant.openingHours;
+console.log(weekdays2);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "onions", "olives", "spinah");
+restaurant.orderPizza("mushrooms");
