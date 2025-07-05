ab2a9f963ad85f2b9aeb236346e04b654365f2c8
jonathan_fambro <startek591@gmail.com>
2025-07-05 00:23:24 -0400

    Looping Objects: Object Keys, Values and Entries

--- 07-Data-Structures-Operators/script.js
+++ 07-Data-Structures-Operators/script.js
@@ -316,3 +316,28 @@ ordersSet.add("Gralic Bread");
 console.log(ordersSet);
 
 for (const order of ordersSet) console.log(order);
+
+// Looping Objects: Object Keys, Values and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
